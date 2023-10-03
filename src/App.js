301caf1e67b92980c84e5b50d7960d5b812babaf
Samir Jadhav <samirzjadhav@gmail.com>
2023-10-03 10:14:34 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { HiChevronDown } from "react-icons/hi";
+import { HiArrowNarrowRight } from "react-icons/hi";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className=" w-auto flex justify-center items-center gap-[40px] p-[20px]">
+        <div className="logo">
+          <a href="logo-heading">Submit War</a>
+        </div>
+        <div className="nav-content flex items-center justify-between gap-[20px]">
+          <a href="">Home</a>
+          <a href="">Feature</a>
+          <a href="">Pricing</a>
+          <a href="">Customers</a>
+          <a href="">ToolKit</a>
+          <a href="">Blog</a>
+          <div className="flex items-center">
+            <a href="">Resources</a>
+            <HiChevronDown />
+          </div>
+        </div>
+        <div className="sign-up flex gap-[30px] pl-[30px]">
+          <a href="">Sign in</a>
+          <div className="flex items-center">
+            <button>Get started </button>
+            <HiArrowNarrowRight />
+          </div>
+        </div>
+      </nav>
+    </>
   );
 }
 
